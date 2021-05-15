23b6f0809e3b8c34eb7f2b79c67365285a9fa8c2
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2021-05-15 21:07:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,55 @@
-//let root, menuBtn, navEl, navlinks, go2Top, go2TopBtn;
+let root,
+	menuBtn,
+	navEl,
+	navlinks,
+	go2Top,
+	go2TopBtn,
+	dsp,
+	prevYPos = -1,
+	currYPos,
+	flag = false;
 
-const root = document.documentElement;
-const menuBtn = document.getElementById("menu");
-const navEl = document.getElementById("nav_el");
-const navLinks = document.querySelectorAll("#nav_el a");
-const go2Top = document.getElementById("back2top");
-const go2TopBtn = document.querySelector("#back2top button");
+window.addEventListener("DOMContentLoaded", () => {
+	init();
+	window.addEventListener("scroll", () => {
+		currYPos = window.pageYOffset;
+		if (prevYPos < currYPos && currYPos > 600 && !flag) {
+			go2Top.style.display = "flex";
+			flag = !flag;
+		} else if (prevYPos > currYPos && currYPos < 600 && flag) {
+			go2Top.style.display = "none";
+			flag = !flag;
+		}
+		prevYPos = currYPos;
+		/*
+  if(window.pageYOffset > 600) go2Top.style.display = 'block';
+  else go2Top.style.display = 'none';
+  */
+	});
+	go2TopBtn.addEventListener("click", () => {
+		root.scrollTop = 0;
+	});
 
-menuBtn.addEventListener("click", () => {
-  menuBtn.children[0].children[0].classList.toggle("cross");
-  navEl.classList.toggle("open");
+	menuBtn.addEventListener("click", () => {
+		menuBtn.children[0].children[0].classList.toggle("cross");
+		navEl.classList.toggle("open");
+	});
+
+	navLinks.forEach((el) => {
+		el.addEventListener("click", () => {
+			menuBtn.click();
+		});
+	});
 });
 
-navLinks.forEach((el) => {
-  el.addEventListener("click", () => {
-    menuBtn.click();
-  });
-});
-
-
-window.addEventListener("scroll", () => {
-  let dsp = window.pageYOffset > 600 ? "block" : "none";
-  //console.log(window.pageYOffset);
-  go2Top.style.display = dsp;
-  /*
-if(window.pageYOffset > 600) go2Top.style.display = 'block';
-else go2Top.style.display = 'none';
-*/
-});
-go2TopBtn.addEventListener("click", () => {
-  root.scrollTop = 0;
-});
+function init() {
+	root = document.documentElement;
+	menuBtn = document.getElementById("menu");
+	navEl = document.getElementById("nav_el");
+	navLinks = document.querySelectorAll("#nav_el a");
+	go2Top = document.getElementById("back2Top");
+	go2TopBtn = document.querySelector("#back2Top button");
+}
 
 function sleep(ms) {
 	return new Promise((resolve) => setTimeout(resolve, ms));
